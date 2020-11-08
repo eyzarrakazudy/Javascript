a1c630858078a3e0f5147cc52cb9f7bce9c0ca84
eyzar rsm <eyzar.rsm@gmail.com>
2020-11-08 01:04:23 -0800

    membuat alert

--- latihan4/script.js
+++ latihan4/script.js
@@ -1,9 +1,12 @@
-alert('Hello World!')
-var nama = prompt('masukkan nama')
-alert(nama)
-var tes = confirm('kamu yakin?')
-if( tes === true ) {
-    alert('user menekan OK!')
-} else {
-    alert('user menekan CANCEL!')
+alert('selamat datang')
+
+var lagi = true;
+
+while( lagi === true) {
+    var nama = prompt('masukkan nama:')
+    alert('halo' + nama);
+
+    lagi = confirm('coba lagi?')
 }
+
+alert('terima kasih..')
