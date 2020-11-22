c30214c6794dabe8522767f4d89cad2015c3dafb
eyzar rsm <eyzar.rsm@gmail.com>
2020-11-22 05:47:59 -0800

    mencoba cara cara penulisan string

--- latihan6/repeat.js
+++ latihan6/repeat.js
@@ -1,8 +1,17 @@
 // while(true) {
 //     console.log('Hello World');
 // }
-var ulang = true;
-while(ulang) {
-    console.log('Hello World!');
-    ulang = confirm('lagi?')
+// var ulang = true;
+// while(ulang) {
+//     console.log('Hello World!');
+//     ulang = confirm('lagi?')
+// }
+var nilaiawal = 1;
+while(nilaiawal <= 10) {
+    // console.log('Hello World '+nilaiawal +'x');
+    // console.log('Angkot No. '+nilaiawal +' beroperasi dengan baik.');
+    // console.log(`Angkot No. `+nilaiawal +` beroperasi dengan baik.`);
+    console.log(`Angkot No. ${nilaiawal} beroperasi dengan baik.`);
+    // nilaiawal++;
+    nilaiawal = nilaiawal+1
 }
