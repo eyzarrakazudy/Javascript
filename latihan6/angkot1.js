4b198fbc2ac976f181ee63194601e1dd9b0f458e
eyzar rsm <eyzar.rsm@gmail.com>
2020-11-28 05:44:11 -0800

    menambahkan file baru

---
+++ latihan6/angkot1.js
@@ -0,0 +1,13 @@
+// var jmlAngkot = prompt('jumlah angkot sekarang:')
+var jmlAngkot = 10
+var noAngkot = 1
+
+while(noAngkot <= jmlAngkot) {
+    // console.log('Hello World '+noAngkot +'x')
+    console.log('Angkot No. '+noAngkot +' beroperasi dengan baik.')
+    // console.log(`Angkot No. `+noAngkot +` beroperasi dengan baik.`)
+    // console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`)
+    // alert(`Angkot No. ${noAngkot} beroperasi dengan baik.`)
+    noAngkot++
+    // noAngkot = noAngkot+1
+}
--- latihan6/repeat.js
+++ latihan6/repeat.js
@@ -1,17 +1,8 @@
 // while(true) {
 //     console.log('Hello World');
 // }
-// var ulang = true;
-// while(ulang) {
-//     console.log('Hello World!');
-//     ulang = confirm('lagi?')
-// }
-var nilaiawal = 1;
-while(nilaiawal <= 10) {
-    // console.log('Hello World '+nilaiawal +'x');
-    // console.log('Angkot No. '+nilaiawal +' beroperasi dengan baik.');
-    // console.log(`Angkot No. `+nilaiawal +` beroperasi dengan baik.`);
-    console.log(`Angkot No. ${nilaiawal} beroperasi dengan baik.`);
-    // nilaiawal++;
-    nilaiawal = nilaiawal+1
+var ulang = true;
+while(ulang) {
+    console.log('Hello World!');
+    ulang = confirm('lagi?')
 }
