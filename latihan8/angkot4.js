26e9b6a2dbe53606b08eba172dbd475fd6ac65b8
eyzar rsm <eyzar.rsm@gmail.com>
2020-12-13 00:05:29 -0800

    menambahkan or satu lagi

--- latihan8/angkot4.js
+++ latihan8/angkot4.js
@@ -6,7 +6,7 @@ while( lagi ) {
     for ( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
         if( noAngkot <= 6 ) {
             console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik.`)
-        } else if( noAngkot === 8 || noAngkot === 10 ) {
+        } else if( noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
             console.log(`Angkot No. ${noAngkot} Sedang lembur.`)            
         } else {
             console.log (`Angkot No. ${noAngkot} sedang tidak beroperasi.`)
