47e699bd9fd780415159731f5800e0f9e545c328
eyzar rsm <eyzar.rsm@gmail.com>
2020-12-13 00:12:09 -0800

    membenarkan kesalahan

--- latihan8/angkot4.js
+++ latihan8/angkot4.js
@@ -4,7 +4,7 @@ var angkotBeroperasi = 6
 
 while( lagi ) {
     for ( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
-        if( noAngkot <= 6 && noAngkot !== 5) {
+        if( noAngkot <= 6 && noAngkot !== 5 ) {
             console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik.`)
         } else if( noAngkot === 8 || noAngkot === 10 || noAngkot === 5 ) {
             console.log(`Angkot No. ${noAngkot} Sedang lembur.`)            
