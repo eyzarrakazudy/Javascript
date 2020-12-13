0a29e523b749081f6cd6cf1c82987138dc5f487e
eyzar rsm <eyzar.rsm@gmail.com>
2020-12-12 23:35:23 -0800

    mengulangi dengan cara yang efektif

--- latihan8/angkot4.js
+++ latihan8/angkot4.js
@@ -6,10 +6,8 @@ while( lagi ) {
     for ( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
         if( noAngkot <= 6 ) {
             console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik.`)
-        } else if(noAngkot === 8) {
+        } else if( noAngkot === 8 || noAngkot === 10 ) {
             console.log(`Angkot No. ${noAngkot} Sedang lembur.`)            
-        } else if(noAngkot === 10) {
-            console.log(`Angkot No. ${noAngkot} Sedang lembur.`)
         } else {
             console.log (`Angkot No. ${noAngkot} sedang tidak beroperasi.`)
         }
