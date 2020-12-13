23829eda21ddddcef97ee41f6c6453b485657837
eyzar rsm <eyzar.rsm@gmail.com>
2020-12-12 23:24:19 -0800

    menambahkan else if

--- latihan8/angkot4.js
+++ latihan8/angkot4.js
@@ -6,6 +6,8 @@ while( lagi ) {
     for ( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
         if( noAngkot <= 6 ) {
             console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik.`)
+        } else if(noAngkot === 8) {
+            console.log(`Angkot No. ${noAngkot} Sedang lembur.`)            
         } else {
             console.log (`Angkot No. ${noAngkot} sedang tidak beroperasi.`)
         }
