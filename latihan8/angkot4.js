dd4a7b1d158fa899fbc707ddee760972bc95a375
eyzar rsm <eyzar.rsm@gmail.com>
2020-12-13 00:10:40 -0800

    menambahkan operator logis and dan not

--- latihan8/angkot4.js
+++ latihan8/angkot4.js
@@ -4,9 +4,9 @@ var angkotBeroperasi = 6
 
 while( lagi ) {
     for ( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
-        if( noAngkot <= 6 ) {
+        if( noAngkot <= 6 && noAngkot !== 5) {
             console.log(`Angkot No. ${noAngkot} Beroperasi dengan baik.`)
-        } else if( noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
+        } else if( noAngkot === 8 || noAngkot === 10 || noAngkot === 5 ) {
             console.log(`Angkot No. ${noAngkot} Sedang lembur.`)            
         } else {
             console.log (`Angkot No. ${noAngkot} sedang tidak beroperasi.`)
