15f7dbf9d027e8049c24cdc658cb1c8f94f62888
eyzar rsm <eyzar.rsm@gmail.com>
2020-12-15 21:12:19 -0800

    membenarkan kesalahan

--- css-effect/index.js
+++ css-effect/index.js
@@ -1,11 +1,11 @@
-const menuBtn = document.querySelector('.menu-btn')
-let menuOpen = false
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
-        menuBtn.classList.add('open')
-        menuOpen = true
+        menuBtn.classList.add('open');
+        menuOpen = true;
     } else {
-        menuBtn.classList.remove('open')
-        menuOpen = false
+        menuBtn.classList.remove('open');
+        menuOpen = false;
     }
 }); 
