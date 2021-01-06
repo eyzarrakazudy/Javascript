6cf372b7a0e8c7e17047bf54122c8867318ff5e6
eyzar rsm <eyzar.rsm@gmail.com>
2021-01-05 23:37:32 -0800

    menambahkan koma

--- latihan9/switch2.js
+++ latihan9/switch2.js
@@ -1,13 +1,13 @@
 var angka = prompt('masukkan angka : ')
 
 switch( angka ) {
-    case 1 :
+    case '1' :
         alert('anda memasukkan angka 1')
         break
-    case 2 :
+    case '2' :
         alert('anda memasukkan angka 2')
         break
-    case 3 : 
+    case '3' : 
         alert('anda memasukkan angka 3')
         break
     default :
