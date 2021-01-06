c6c3f0dec891bc4fcfd3f17b81da987c9c7363d1
eyzar rsm <eyzar.rsm@gmail.com>
2021-01-05 23:54:49 -0800

    menyelesaikan pr switch

---
+++ latihan9/switchfood.js
@@ -0,0 +1,21 @@
+var item = prompt('masukkan nama makanan / minuman : \n (contoh: nasi,daging,susu,hamburger,softdrink)')
+
+switch( item ) {
+    case 'nasi' :
+        alert('makanan / minuman SEHAT!')
+        break
+    case 'daging' :
+        alert('makanan / minuman SEHAT!')
+        break
+    case 'susu' :
+        alert('makanan / minuman SEHAT!')
+        break
+    case 'hamburger' :
+        alert('makanan / minuman TIDAK SEHAT!')
+        break
+    case 'softdrink' :
+        alert('makanan / minuman TIDAK SEHAT!')
+    default :
+        alert('anda memasukkan nama makanan / minuman yang salah!')
+        break                    
+}
