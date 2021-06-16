8d58691e8412973c00b67251e36da270b154e379
unknown <eyzar.rsm@gmail.com>
2021-06-16 05:56:27 -0700

    menyelesaikan kode untuk suwit jawa

---
+++ latihan12/suwit.js
@@ -0,0 +1,35 @@
+// menangkap pilihan player
+var p = prompt('pilih : gajah, semut, orang')
+
+// menangkap pilihan komputer
+// membangkitkan bilangan random
+var comp = Math.random()
+
+if(comp < 0.34 ) {
+    comp = 'gajah'
+} else if( comp >= 0.34 && comp < 0.67 ) {
+    comp = 'orang'
+} else {
+    comp = 'semut'
+}
+
+var hasil = ''
+// menentukan rules
+if( p == comp ) {
+    hasil = 'SERI!'
+} else if( p == 'gajah') {
+   // if( comp == 'orang' ) {
+   //     hasil = 'MENANG!'
+  //  } else {
+   //    hasil = 'KALAH!'
+  //  }
+  hasil = ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!'
+} else if( p == 'orang' ) {
+    if( comp == 'gajah' ) {
+        hasil = 'KALAH!'
+    } else {
+        hasil = 'MENANG!'
+    }
+}
+
+// tampilkan hasilnya
