3ee1243cba70d74c62308e4620c74f4ed9d716c8
unknown <eyzar.rsm@gmail.com>
2021-06-21 04:04:42 -0700

    menambahkan file argu-para-old.js

---
+++ latihan14/argu-para-old.js
@@ -0,0 +1,8 @@
+function tambah(a,b) {
+    return a + b
+}
+
+var a = parseInt(prompt('masukkan nilai 1 :'))
+var b = parseInt(prompt('masukkan nilai 2 :'))
+var hasil = tambah(a*2,b*2)
+console.log(hasil)
--- latihan14/argu-para.js
+++ latihan14/argu-para.js
@@ -1,5 +1,11 @@
 function tambah(a,b) {
     return a + b
 }
-var hasil = tambah(2,3)
+
+function kali(a,b) {
+    return a * b
+}
+
+
+var hasil = kali (tambah(1,2), tambah(3,4))
 console.log(hasil)
