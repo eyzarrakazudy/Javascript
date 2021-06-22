bc432bd75f27569b14ae6107d729ad110a45608f
unknown <eyzar.rsm@gmail.com>
2021-06-22 04:21:31 -0700

    menambahkan file old lagi untuk function.js dan membenarkan file function.js

--- latihan13/function.js
+++ latihan13/function.js
@@ -1,14 +1,5 @@
 function jumlahVolumeDuaKubus(a, b) {
-    var volumeA
-    var volumeB
-    var total
-
-    volumeA = a * a * a
-    volumeB = b * b * b
-
-    total = volumeA + volumeB
-
-    return total
+    return a * a * a + b * b * b
 }
 
 alert(jumlahVolumeDuaKubus(8, 3))
