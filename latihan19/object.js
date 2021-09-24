da78c2b14e8a8b7b3228c8f994bf3e7dbd154592
unknown <eyzar.rsm@gmail.com>
2021-09-24 04:15:29 -0700

    menyelesaikan video object

---
+++ latihan19/object.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama : "Sandhika Galih",
+    umur : 31,
+    ips : [3.00,2.50,3.20],
+alamat : {
+    jalan : "JL. abc No. 123",
+    kota : "Bandung",
+    provinsi : "Jawa barat",
+  }
+}
